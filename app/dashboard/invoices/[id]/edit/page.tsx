932bcf1bbe69035d166d07ab169ec743e0bf9645
thom99 <thomas@damialfredo.com>
2025-06-04 15:55:19 +0200

    Handling Errors, Accessibility and Next auth
    Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-import { notFound } from 'next/navigation';
+import NotFound from './not-found';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -12,7 +17,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   ]);
   
   if (!invoice) {
-    notFound();
+    NotFound();
   }
   return (
     <main>
